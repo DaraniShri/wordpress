ade1b78d0d9fad79f40c95063658107027bbde03
DaraniShri <daranirameshbabu@gmail.com>
2023-08-02 18:25:35 +0530

    adjust(wordpress):meta values are stored, custom post created, creating archive page

--- wordpress/wp-content/themes/ws-custom-theme/functions.php
+++ wordpress/wp-content/themes/ws-custom-theme/functions.php
@@ -1,10 +1,41 @@
 <?php
-    function hook_javascript() {
-        ?>
-            <script>
-                alert('Hello world...');
-            </script>
-        <?php
-    }
     add_action('wp_head', 'hook_javascript');
+
+    add_filter( 'the_content', 'change_content' );
+
+    function hook_javascript() {
+        echo "Hello world...";
+        echo "This is an action hook";
+    }
+
+    function change_content ( $content ) {
+        $content = 'This is a filter hooks';
+        return $content;
+    }
+
+    add_filter('writeMeta','storeMetaPost',10,4);
+    function storeMetaPost( $post_id, $meta_key, $meta_value, $unique = false ) {
+        $the_post = wp_is_post_revision( $post_id );
+        if ( $the_post ) {
+            $post_id = $the_post;
+        }
+    
+        return add_metadata( 'post', $post_id, $meta_key, $meta_value, $unique );
+    }
+
+    add_action( 'init', 'register_news_cpt' );
+    function register_news_cpt() {
+        register_post_type( 'news',array(
+                                    'labels' => 
+                                        array(
+                                            'name' => __( 'News' ),
+                                            'singular_name' => __( 'News' ),
+                                            'description' => __('Independent sources'),
+                                        ),
+                                        'public' => true,
+                                        'has_archive' => false,
+                                        'rewrite' => array('slug' => 'news'),
+                                    )
+                            );
+        }
 ?>
--- wordpress/wp-content/themes/ws-custom-theme/index.php
+++ wordpress/wp-content/themes/ws-custom-theme/index.php
@@ -1,1 +1,6 @@
 <h1>hello world!</h1>
+<?php
+    wp_head();
+    the_content();
+    $addMetaValues=apply_filters('writeMeta',1,'meta_description','post meta value',false);
+?>
